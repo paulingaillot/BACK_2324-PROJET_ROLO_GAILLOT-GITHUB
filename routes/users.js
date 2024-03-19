a68c2ec1b1a669cd0e8b17a0df9b4c1a9a31157d
Paulin <paulingaillot@gmail.com>
2024-03-19 13:50:33 +0100

    BRG-7 - Modification Login$

--- routes/users.js
+++ routes/users.js
@@ -88,7 +88,7 @@ router.post('/login', async (req, res) => {
 
             }
 
-            res.status(200).json({ message: 'Login successful', user: user });
+            res.status(200).json({ message: 'Login successful', user: user.toObject() });
         }
 
     } catch (error) {
